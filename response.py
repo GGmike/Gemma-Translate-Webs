bfa10d63fdd93726de66459112ca3e0eca00fb39
GGmike <miketsun1212@gmail.com>
2026-02-28 00:57:48 +0800

    Add new format

--- response.py
+++ response.py
@@ -3,3 +3,12 @@ from pydantic import BaseModel
 class TranslationResponse(BaseModel):
     translated_text: str
     reasoning: str
+
+
+class TranslationInput(BaseModel):
+    text: str
+    source_lang: str
+    target_lang: str
+    source_lang_code: str
+    target_lang_code: str
+    model: str
