349feacf7f9fd7e98fb5451ddc129294b3bbafb8
GGmike <miketsun1212@gmail.com>
2026-02-28 00:58:34 +0800

    Update main translator services

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, jsonify
-from services import Translator
+from services.Translator import Translator
 from config import AVAILABLE_MODELS
+from response import TranslationInput
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="templates/static", template_folder="templates")
 
 @app.route("/")
-def home():
+def home():    
     return render_template("home.html")
 
 @app.route("/models", methods=["GET"])
@@ -16,18 +17,29 @@ def get_models():
 def translate():
     inputs = request.get_json()
     print(f"Received translation request: {inputs}")
-    
-    text = inputs.get("text", "")
-    source_lang = inputs.get("source_lang", "en")
-    target_lang = inputs.get("target_lang", "cn")
-    model = inputs.get("model", "translategemma:4b")  
+    try:
+        text = inputs.get("text", "")
+        source_lang = inputs.get("source_label", "English")
+        target_lang = inputs.get("target_label", "Chinese")
+        model = inputs.get("model", "translategemma:4b")  
+        source_lang_code = inputs.get("source_lang", "en")
+        target_lang_code = inputs.get("target_lang", "cn")
+        translation_input = TranslationInput(
+        text=text,
+        source_lang=source_lang,
+        target_lang=target_lang,
+        source_lang_code=source_lang_code,
+        target_lang_code=target_lang_code,
+        model=model
+        )
+
+    except Exception as e:
+        print(f"Error parsing input: {e}")
+        return jsonify({"error": "Invalid input format"}), 400
     
     translator = Translator()
-    # TODO: pass model to translator
-    # translated = translator.translate(text, source_lang, target_lang, model)
+    translated = translator.translate(translation_input)
 
-    # Placeholder response
-    translated = f"[Translation of '{text}' from {source_lang} to {target_lang} using {model}]"
 
     return jsonify({"translated_text": translated})
 
--- services/Translator.py
+++ services/Translator.py
@@ -1,22 +1,52 @@
 from langchain_core.tools import BaseTool
+from typing import Optional
 from langchain_ollama import OllamaLLM
 from response import TranslationResponse
 from config import LLMConfig
+from response import TranslationInput, TranslationResponse
+
+
+
 class Translator(BaseTool):
     name: str = "Translator"
     description: str = "A tool for translating text between languages using a specified model."
+    inputs: Optional[TranslationInput] = None
+    _llm : Optional[OllamaLLM] = None
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._llm = self._init_llm()
+        # self._llm = self._init_llm()
     
     def _init_llm(self) -> OllamaLLM:
         _llm_config = LLMConfig()
         return OllamaLLM(
-            model=_llm_config.model_name,
+            model=self.inputs.model,
             temperature=_llm_config.temperature,
             verbose=_llm_config.verbose,
             base_url=_llm_config.base_url
         )
     
     def _run(self) -> TranslationResponse:
-        pass
+        if self.inputs is None:
+            raise ValueError("TranslationInput is required to run the translator.")
+        prompt = self._build_translation_prompt()
+        response = self._llm.invoke(
+            prompt,
+            format=TranslationResponse.model_json_schema()
+            )
+        return TranslationResponse.model_validate_json(response)
+
+    def _build_translation_prompt(self) -> str:
+        prompt =  f"""
+        You are a professional {self.inputs.source_lang} ({self.inputs.source_lang_code}) to {self.inputs.target_lang} ({self.inputs.target_lang_code}) translator. Your goal is to accurately convey the meaning and nuances of the original {self.inputs.source_lang} text while adhering to {self.inputs.target_lang} grammar, vocabulary, and cultural sensitivities.
+        Produce only the {self.inputs.target_lang} translation, without any additional explanations or commentary. Please translate the following {self.inputs.source_lang} text into {self.inputs.target_lang}:
+
+        {self.inputs.text}
+        """
+        print(f"Constructed prompt: {prompt}")
+        return prompt
+
+    def translate(self, translation_input: TranslationInput) -> str:
+        self.inputs = translation_input
+        self._llm = self._init_llm()
+        response = self._run()
+        return response.translated_text.strip()
